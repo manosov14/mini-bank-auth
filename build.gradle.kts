e28a58f2f92cbcb9017d7ac3ed32640d48b52c2f
Mikhail <manosov14@gmail.com>
2023-09-29 15:46:46 +0300

    change gradle settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ group = "com.example"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_11
 }
 
 repositories {
@@ -47,7 +47,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs += "-Xjsr305=strict"
-        jvmTarget = "17"
+        jvmTarget = "11"
     }
 }
 
