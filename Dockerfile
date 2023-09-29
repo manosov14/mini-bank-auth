348e8ec6a0e287d636010f342f7229f479c7dd2d
Mikhail <manosov14@gmail.com>
2023-09-29 15:41:23 +0300

    added docker configuration

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM openjdk:11-jdk-slim
 VOLUME /tmp
 ARG JAR_FILE
-COPY build/libs/mini-bank-0.0.1-SNAPSHOT.jar mini-bank-0.0.1-SNAPSHOT.jar
+COPY build/libs/mini-bank-0.0.1-SNAPSHOT.jar ./
+EXPOSE 8000
 ENTRYPOINT ["java","-jar","/mini-bank-0.0.1-SNAPSHOT.jar"]
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.3"
     kotlin("jvm") version "1.8.22"
     kotlin("plugin.spring") version "1.8.22"
-    kotlin("plugin.jpa") version "1.8.22"
+    id("java")
 }
 
 group = "com.example"
@@ -19,14 +19,15 @@ repositories {
     mavenCentral()
 }
 
- dependencies {
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("org.postgresql:postgresql")
-//    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
